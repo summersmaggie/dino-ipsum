8da088c05e1bbeaa0fee2f01df298bcc93dc0843
Hannah Lee and Maggie Summers <maggie@>
2018-03-22 14:20:45 -0700

    add GIPHY API.

--- src/js/dino.js
+++ src/js/dino.js
@@ -14,4 +14,21 @@ export class Dino {
       request.send();
     });
   }
+
+  getGiphyAPI() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.giphy.com/v1/gifs/random?q=dinosaur&api_key=${process.env.GIPHY_KEY}&limit=5`;
+      console.log('url '+url);
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { Dino } from './js/dino.js'
 
 $(document).ready(function() {
   $('#dino-ipsum').click(function() {
-
+    $('#dino-ipsum').hide();
     let newDino = new Dino();
 
     let promise = newDino.getDinoAPI();
@@ -13,7 +13,16 @@ $(document).ready(function() {
         for (let i = 0; i < body.length; i++) {
         $('.show-dino-ipsum').append(" " + "<li>" + body[i] + "</li>");
       }
+
+    let promiseSecond = newDino.getGiphyAPI();
+
+    promiseSecond.then(function(response) {
+      let parsedBody = JSON.parse(response);
+      // let random = Math.floor(Math.random()* 4);
+      let reply = parsedBody.data.images.original.url;
+      $('.show-dino-giphy').html(`<img src=${reply}>`);
       });
     });
   });
+});
 });
