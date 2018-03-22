0f21862e62ea6ea5b3cfe4cef0dbb202cef8366a
Hannah Lee and Maggie Summers <maggie@>
2018-03-22 13:15:32 -0700

    add for loop to promise on main.js.

--- src/main.js
+++ src/main.js
@@ -9,10 +9,11 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      body.forEach(function(element) {
-        $('.show-dino-ipsum').append("<ol>" + element + "</ol>");
+      body.forEach(function(body) {
+        for (let i = 0; i < body.length; i++) {
+        $('.show-dino-ipsum').append(" " + "<li>" + body[i] + "</li>");
+      }
       });
     });
-
   });
 });
